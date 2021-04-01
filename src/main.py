c5b045a086e28991dd820fc09b36a858ddb2b291
Eric Lopez <e_lo28@yahoo.com>
2021-03-31 22:46:32 -0700

    working base model

--- src/main.py
+++ src/main.py
@@ -1,13 +1,78 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
-from keras.optimizers import Adam
+import random
+from collections import deque
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+from tensorflow.keras.optimizers import Adam
 
-N_SEQ = 0
-TIMESTEPS = 0
-N_FEATURES = 0
 
-print(tf.__version__)
+n_steps_in = 60 # one sequence will have 60 elements of data
+n_steps_out = 5 # goal is to predict 5 elements of data 
+
+def is_profit(actual, future):
+	if float(future) > float(actual):
+		return 1
+	return 0
+
+def preprocess_df(df):
+	df = df.drop(columns='future')
+	df[['close']] = df[['close']].apply(lambda x:(x-x.min()) / (x.max()-x.min()))
+	sequence_list = list()
+	sequence = deque(maxlen=n_steps_in)
+	for row in df.values:
+		sequence.append([col for col in row[:-1]])
+		if len(sequence) == n_steps_in:
+			sequence_list.append([sequence, row[-1]])
+	profit = list(); loss = list()
+	for seq, target in sequence_list:
+		if target == 1:
+			profit.append([seq, target])
+		elif target == 0:
+			loss.append([seq, target])
+	balance = min(len(profit), len(loss))
+	profit = profit[:balance]
+	loss = loss[:balance]
+	sequence_list = profit + loss
+	random.shuffle(sequence_list)
+	x = list(); y = list()
+	for seq, target in sequence_list:
+		x.append(np.array(seq))
+		y.append(target)
+	return np.array(x), np.array(y)
+
+
+df = pd.read_csv("./src/data/BTC-USD.csv")
+df.set_index('time', inplace=True)
+df.sort_index(inplace=True)
+df.drop(columns=['open', 'high', 'low', 'volume'], inplace=True)
+
+# create future column, create target/label column
+df['future'] = df['close'].shift(-n_steps_out)
+df['target'] = list(map(is_profit, df['close'], df['future']))
+
+# split data into train, test sections (70/30) split
+timestamp = df.iloc[-int(df.shape[0] * 0.3)]
+train_df = df.loc[df.index < timestamp.name]
+test_df = df.loc[df.index >= timestamp.name]
+
+# preprocess_df
+	# normalize and scale columns
+	# balance data (shuffle and balance profits and losses
+	# split sequences from labels
+x_train, y_train = preprocess_df(train_df)
+x_test, y_test = preprocess_df(test_df)
+
+# pass data through neural network.
+model = Sequential()
+
+model.add(LSTM(64))
+model.add(Dropout(0.2))
+
+model.add(Dense(n_steps_out))
+
+opt = Adam(lr=0.005, decay=1e-6)
+model.compile(optimizer=opt, loss='mse', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=8, verbose=1)
 
